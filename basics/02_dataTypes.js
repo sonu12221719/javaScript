8c50f287656dad87662f2dee5e7f053d53c1342e
sonu kumar <113254777+sonu12221719@users.noreply.github.com>
2023-12-12 10:44:59 +0000

    Learn about dataTypes in JS

---
+++ basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+//alert("hello") this will show in web browser not in console or not in node js
+
+//we cann't use statement like that
+//console.log() console.log()
+//for using two different statement in sameline we use ';' for separation it is good practice to use ';' in every statement end.
+//And semicolon also increase readability of the code
+
+//Some frequently used dataTypes are
+
+//number => 2 power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => stand alone value or will printing its dataType in console it gives "object"
+//undefined => when no any value is defined to the variable
+//symbol => unique
+
+//All the avobe are PRIMITIVE dataTypes
+
+//object => important dataType
+
+//now here a keyword "typeof" is used to know the which dataType of variable is.
+
+console.log(typeof "sonu");//string
+console.log(typeof 12);//number
+
+let isStudy = true;
+console.log(typeof isStudy);//boolean
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+//we will see symbol dataType later.
+
+
